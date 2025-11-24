4d8cfeaa54f89fd6ad3ff06d4eda80c8d6af263c
Naxomega.sh <124938856+Naxomega@users.noreply.github.com>
2025-11-24 07:17:12 +0100

    lil' update
    Just some optimizations and tweaks to make it work better with KDE

--- compteur.py
+++ compteur.py
@@ -9,8 +9,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 var = 0
-var2 = 5
-var3 = 0
+
 
 class MyWindow(Gtk.Window):
 
@@ -22,17 +21,18 @@ class MyWindow(Gtk.Window):
         # Create a Box container to hold widgets
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.add(box)  # Add the box to the window
+        self.set_size_request(190, 30)
 
         self.button = Gtk.Button(label="-")
         self.button.connect("clicked", self.moins)
-        self.button.set_size_request(40, 30)  # Width: 40 pixels, Height: 30 pixels
+        self.button.set_size_request(70, 30)  # Width: 40 pixels, Height: 30 pixels
         box.pack_start(self.button, False, False, 0)  # Don't expand or fill
 
         self.label = Gtk.Label(label="0", angle=0, halign=Gtk.Align.CENTER)
         box.pack_start(self.label, True, True, 0)  # Add label to the box
         self.button1 = Gtk.Button(label="+")
         self.button1.connect("clicked", self.plus)
-        self.button1.set_size_request(40, 30)  # Same size as the other button
+        self.button1.set_size_request(70, 30)  # Same size as the other button
         box.pack_start(self.button1, False, False, 0)  # Don't expand or fill
         
 
